c6fd1a75838e1bf84fd0620a5989890db647e348
CalvoM <mwadimemakokha@ieee.org>
2020-09-13 18:59:36 +0300

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'cb873767d840b963da0bd784bebb9619'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '9d9e55a1-4c64-4867-9ce6-fa5e990c246a'
+};
